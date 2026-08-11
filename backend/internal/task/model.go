de35bf5625ed0f402a29f0bc6cda57d05ed94444
nathan <nathan.ta2001@gmail.com>
2026-08-11 10:00:44 -0300

    feat: adiciona opção de adicionar data de incio e/ou fim nas tasks

--- backend/internal/task/model.go
+++ backend/internal/task/model.go
@@ -3,6 +3,7 @@ package task
 import (
 	"errors"
 	"strings"
+	"time"
 )
 
 type TaskStatus string
@@ -14,10 +15,12 @@ const (
 )
 
 type Task struct {
-	ID        int        `json:"id"`
-	Titulo    string     `json:"titulo"`
-	Descricao string     `json:"descricao"`
-	Status    TaskStatus `json:"status"`
+	ID         int        `json:"id"`
+	Titulo     string     `json:"titulo"`
+	Descricao  string     `json:"descricao"`
+	Status     TaskStatus `json:"status"`
+	DataInicio *string    `json:"data_inicio,omitempty"`
+	DataFim    *string    `json:"data_fim,omitempty"`
 }
 
 func (s TaskStatus) IsValid() bool {
@@ -40,6 +43,16 @@ func (t *Task) Validate() error {
 	if !t.Status.IsValid() {
 		return errors.New("Status inválido")
 	}
-	return nil
 
+	// se as duas datas forem fornecidas, data_inicio não pode ser depois da data_fim
+	if t.DataInicio != nil && t.DataFim != nil && *t.DataInicio != "" && *t.DataFim != "" {
+		inicio, errIni := time.Parse(time.RFC3339, *t.DataInicio)
+		fim, errFim := time.Parse(time.RFC3339, *t.DataFim)
+
+		if errIni == nil && errFim == nil && inicio.After(fim) {
+			return errors.New("a data de fim deve ser superior à data de início")
+		}
+	}
+
+	return nil
 }
