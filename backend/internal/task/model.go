01e0fbca94bc324c65f946f402de4c384f39899f
nathan <nathan.ta2001@gmail.com>
2026-08-11 10:00:01 -0300

    feat: adiciona opçao de colocar datas de inicio e/ou de termino nas tasks

--- backend/internal/task/model.go
+++ backend/internal/task/model.go
@@ -1,5 +1,10 @@
 package task
 
+import (
+	"errors"
+	"strings"
+)
+
 type TaskStatus string
 
 const (
@@ -13,4 +18,28 @@ type Task struct {
 	Titulo    string     `json:"titulo"`
 	Descricao string     `json:"descricao"`
 	Status    TaskStatus `json:"status"`
+}
+
+func (s TaskStatus) IsValid() bool {
+
+	switch s {
+	case StatusAfazer, StatusEmprogresso, StatusFeito:
+		return true
+	default:
+		return false
+	}
+
+}
+
+func (t *Task) Validate() error {
+
+	if strings.TrimSpace(t.Titulo) == "" {
+		return errors.New("A tarefa deve ter um Título")
+	}
+
+	if !t.Status.IsValid() {
+		return errors.New("Status inválido")
+	}
+	return nil
+
 }
